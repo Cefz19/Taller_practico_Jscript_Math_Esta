2bc8d76cecaaaa84a6ad7d77878bb418cebda8d3
Jonhy Armando Hernadez <merano_diseno3@PROD.GRUPOYUYIN.COM>
2023-03-18 13:39:44 -0600

    v4

---
+++ par_imapr/main.js
@@ -0,0 +1,14 @@
+function esPar(lista) {
+    numero / 2
+
+}
+
+function calcularPromedio(lista) {
+
+    const sumaLista = lista.reduce((a, b) => a + b);
+
+    const promedio = sumaLista / lista.length;
+    console.log(promedio);
+    return promedio;
+
+}
